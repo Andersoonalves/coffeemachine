d93b637b0f3aa8aee1569cc6f88a08d72feed914
Andersoonalves <anderson.alves@dce.ufpb.br>
2014-07-02 20:32:27 -0300

    Implementation: Writing tests for 1.02.

--- src/main/java/br/ufpb/dce/aps/coffeemachine/CoffeeMachineImplementation.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/CoffeeMachineImplementation.java
@@ -2,9 +2,15 @@ package br.ufpb.dce.aps.coffeemachine;
 
 public class CoffeeMachineImplementation implements CoffeeMachine {
 	private final Display display;
+	private final CashBoxLogic cashBoxLogic;
 
 	public CoffeeMachineImplementation(ComponentsFactory factory) {
 		this.display = factory.getDisplay();
 		this.display.info("Insert coins and select a drink!");
+		this.cashBoxLogic = new CashBoxLogic(display);
+	}
+
+	public void insertCoin(Coin coin) {
+		cashBoxLogic.insertCoin(coin);
 	}
 }
---
+++ src/test/java/br/ufpb/dce/aps/coffeemachine/CashBoxLogic.java
@@ -0,0 +1,31 @@
+package br.ufpb.dce.aps.coffeemachine;
+
+public class CashBoxLogic {
+	private final Display display;
+	private final int amountCoins = Coin.values().length;
+	private final int[] coins = new int[amountCoins];
+	private int current;
+
+	public CashBoxLogic(final Display display) {
+		this.display = display;
+	}
+
+	/**
+	 * This method is responsible for insert coins in cashBox.
+	 * 
+	 * @param coin
+	 */
+	public void insertCoin(Coin coin) {
+
+		current += coin.getValue();
+		int i = 0;
+		for (Coin c : Coin.values()) {
+			if (c.equals(coin)) {
+				coins[i]++;
+			}
+			i++;
+		}
+
+		display.info("Total: US$ " + current / 100 + "." + current % 100);
+	}
+}
