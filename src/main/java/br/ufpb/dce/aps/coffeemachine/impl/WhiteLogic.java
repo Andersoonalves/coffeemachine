6e3b1f8f40f147d412af683d2114617d235ae204
rodrigovilar <rvilar@gmail.com>
2014-07-06 01:38:18 -0300

    Task 1.22 finished for COMPOR

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/WhiteLogic.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/WhiteLogic.java
@@ -1,5 +1,6 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import br.ufpb.dce.aps.coffeemachine.Messages;
 import net.compor.frameworks.jcf.api.Component;
 import net.compor.frameworks.jcf.api.Service;
 
@@ -16,7 +17,9 @@ public class WhiteLogic extends Component {
 			return warn;
 		}
 
-		requestService("dispenserContains", MyCoffeeMachine.CREAMER, 0.1);
+		if (! (Boolean) requestService("dispenserContains", MyCoffeeMachine.CREAMER, 0.1)) {
+			return Messages.OUT_OF_CREAMER;
+		}
 
 		return null;
 	}
